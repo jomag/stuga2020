c499bfb42a2ea67f4a9ddc27fadad6bbc3ab2898
Jonatan Magnusson <jonatan.magnusson@above.se>
2019-09-14 23:05:50 +0200

    Add streams and update terminal config

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-import React, { useEffect, useRef } from "react";
-import ReactDOM from "react-dom";
-import "./App.css";
-import "xterm/dist/xterm.css";
-import BasicTerminal from "./BasicTerminal";
+import React, { useEffect, useRef } from 'react';
+import ReactDOM from 'react-dom';
+import './App.css';
+import 'xterm/dist/xterm.css';
+import BasicTerminal from './BasicTerminal';
 
 const ReactBasicTerminal = ({ cols, rows }: { cols: number; rows: number }) => {
   const el = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    console.log("MOUNT!");
+    console.log('MOUNT!');
 
     if (el.current) {
       const options = {
         cols,
-        rows
+        rows,
       };
 
       const term = new BasicTerminal(options);
@@ -31,11 +31,11 @@ const ReactBasicTerminal = ({ cols, rows }: { cols: number; rows: number }) => {
       //   console.log("LINE FEED!");
       // });
     } else {
-      console.error("No container element found");
+      console.error('No container element found');
     }
 
     return () => {
-      console.log("UNMOUNT!");
+      console.log('UNMOUNT!');
     };
   });
 
@@ -45,8 +45,8 @@ const ReactBasicTerminal = ({ cols, rows }: { cols: number; rows: number }) => {
 const App: React.FC = () => {
   return (
     <div>
-      <h1 style={{ margin: "40px 80px 0 80px" }}>Stuga 2020</h1>
-      <div style={{ margin: "40px 80px 40px 80px", border: "4px solid #888" }}>
+      <h1 style={{ margin: '40px 80px 0 80px' }}>Stuga 2020</h1>
+      <div style={{ margin: '40px 80px 40px 80px', border: '4px solid #888' }}>
         <ReactBasicTerminal cols={80} rows={25} />
       </div>
     </div>
--- src/BasicTerminal.ts
+++ src/BasicTerminal.ts
@@ -1,5 +1,6 @@
-import { Terminal, ITerminalOptions } from "xterm";
-import { Stream, Program, Context, setupEnvironment, shell } from "bajsic";
+import { Terminal, ITerminalOptions } from 'xterm';
+import { Stream, Program, Context, setupEnvironment, shell } from 'bajsic';
+import chalk from 'chalk';
 
 type BasicTerminalOptions = {
   cols: number;
@@ -13,51 +14,75 @@ class BasicTerminal {
 
   constructor(options?: BasicTerminalOptions) {
     const { cols, rows } = options || {};
-    let inputBuf = "";
+    let inputBuf = '';
 
     const termOptions: ITerminalOptions = {
       cols,
-      rows
+      rows,
+      cursorBlink: true,
+      cursorStyle: 'underline',
+      fontFamily: 'Px437 IBM EGA8',
+      fontSize: 22,
     };
 
     this.term = new Terminal(termOptions);
 
     this.term.onData((text: string) => {
       this.term.write(text);
+
+      if (text === '\n') {
+        return;
+      }
+
       inputBuf += text;
 
-      if (text === "\r") {
-        this.term.write("\n");
-        console.log("NEW LINE: ", inputBuf);
+      if (text === '\r') {
+        this.term.write('\r\n');
+
         if (this.context && this.context.inputStream) {
           this.context.inputStream.write(inputBuf);
-          inputBuf = "";
+          inputBuf = '';
         }
       }
     });
   }
 
   open(el: HTMLDivElement) {
+    let chalkOptions: any = { enabled: true, level: 2 };
+    const forcedChalk = new chalk.constructor(chalkOptions);
+
     this.term.open(el);
 
-    const env = setupEnvironment("");
+    const env = setupEnvironment();
     this.program = env.program;
     this.context = env.context;
 
+    // Input from user/keyboard to interpreter
     this.context.inputStream = new Stream();
 
+    // Output from interpreter, to be presented on screen
     this.context.outputStream = new Stream();
-    this.context.outputStream.on("data", () => {
-      if (this.context && this.context.outputStream) {
-        const data = this.context.outputStream.read();
-      }
+    this.context.outputStream.on('data', (data: string) => {
+      console.log(`Output stream data: [${data}]`);
+      const data2 = data.replace(/\n/g, '\n\r');
+      this.term.write(data2);
     });
 
-    this.context.outputStream.on("data", (data: string) => {
-      this.term.write(data);
+    // Error from interpreter, to be presented on screen
+    this.context.errorStream = new Stream();
+    this.context.errorStream.on('data', (data: string) => {
+      console.log(`Error stream data: [${data}]`);
+      const data2 = data.replace(/\n/g, '\n\r');
+      this.term.write(forcedChalk.redBright(data2));
     });
 
-    shell(this.program, this.context);
+    fetch('/stuga.bas')
+      .then((r: any) => {
+        console.log(r);
+        return r.text();
+      })
+      .then((r: any) => this.program!.loadFromString(r, this.context!))
+      .then(() => shell(this.program!, this.context!));
   }
 }
 
