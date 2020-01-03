916d46e81e4e849ca5b493416d398433ac906954
Jonatan Magnusson <jonatan.magnusson@above.se>
2020-01-03 12:24:04 +0100

    Terminal I/O fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import React, { useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
 import './App.css';
-import 'xterm/dist/xterm.css';
+import 'xterm/css/xterm.css';
 import BasicTerminal from './BasicTerminal';
 
 const ReactBasicTerminal = ({ cols, rows }: { cols: number; rows: number }) => {
   const el = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    console.log('MOUNT!');
-
     if (el.current) {
       const options = {
         cols,
@@ -18,39 +16,21 @@ const ReactBasicTerminal = ({ cols, rows }: { cols: number; rows: number }) => {
 
       const term = new BasicTerminal(options);
       term.open(el.current);
-
-      // term.onData((text: string) => {
-      //   if (text === "\n") console.log("NEWLINE");
-      //   if (text === "\r") {
-      //     term.write("\n\r");
-      //   }
-      //   console.log(text);
-      //   term.write(text);
-      // });
-      // term.onLineFeed(() => {
-      //   console.log("LINE FEED!");
-      // });
+      term.fit();
     } else {
       console.error('No container element found');
     }
 
     return () => {
-      console.log('UNMOUNT!');
+      alert('unmounted');
     };
   });
 
-  return <div ref={el}></div>;
+  return <div ref={el} style={{ flexGrow: 1 }}></div>;
 };
 
 const App: React.FC = () => {
-  return (
-    <div>
-      <h1 style={{ margin: '40px 80px 0 80px' }}>Stuga 2020</h1>
-      <div style={{ margin: '40px 80px 40px 80px', border: '4px solid #888' }}>
-        <ReactBasicTerminal cols={80} rows={25} />
-      </div>
-    </div>
-  );
+  return <ReactBasicTerminal cols={80} rows={25} />;
 };
 
 export default App;
--- src/BasicTerminal.ts
+++ src/BasicTerminal.ts
@@ -1,5 +1,14 @@
 import { Terminal, ITerminalOptions } from 'xterm';
-import { Stream, Program, Context, setupEnvironment, shell } from 'bajsic';
+import { FitAddon } from 'xterm-addon-fit';
+import {
+  Stream,
+  Program,
+  Context,
+  setupEnvironment,
+  shell,
+  parse,
+  BaseSupport,
+} from 'bajsic';
 import chalk from 'chalk';
 
 type BasicTerminalOptions = {
@@ -7,43 +16,171 @@ type BasicTerminalOptions = {
   rows: number;
 };
 
+const moveLeft = '\x1B[D';
+const moveRight = '\x1B[C';
+
 class BasicTerminal {
   term: Terminal;
   program?: Program;
   context?: Context;
+  fitAddon?: FitAddon;
+  slowDownDelay: number;
+  slowDownBatch: number;
 
   constructor(options?: BasicTerminalOptions) {
     const { cols, rows } = options || {};
     let inputBuf = '';
 
+    this.slowDownDelay = 0;
+    this.slowDownBatch = 3;
+
     const termOptions: ITerminalOptions = {
-      cols,
-      rows,
+      // cols,
+      // rows,
       cursorBlink: true,
       cursorStyle: 'underline',
       fontFamily: 'Px437 IBM EGA8',
-      fontSize: 22,
+      fontSize: 16,
+      theme: {
+        foreground: '#ddd',
+      },
     };
 
     this.term = new Terminal(termOptions);
+    this.fitAddon = new FitAddon();
+    this.term.loadAddon(this.fitAddon);
 
-    this.term.onData((text: string) => {
+    //this.term.onData((text: string) => {
+    //  this.term.write(text);
+    // });
+  }
+
+  fit() {
+    this.fitAddon!.fit();
+  }
+
+  async print(text: string): Promise<void> {
+    if (this.slowDownDelay === 0) {
       this.term.write(text);
+    } else {
+      return new Promise((resolve, reject) => {
+        let remaining = text;
+        const ref = setInterval(() => {
+          if (remaining) {
+            // console.log('WRITE: ', remaining[0]);
+            this.term.write(remaining.slice(0, this.slowDownBatch));
+            remaining = remaining.slice(this.slowDownBatch);
+            // console.log(JSON.stringify(remaining));
+          }
+          if (!remaining) {
+            clearInterval(ref);
+            resolve();
+          }
+        }, this.slowDownDelay);
+      });
+    }
+  }
 
-      if (text === '\n') {
-        return;
-      }
+  async readInput() {
+    let buf = '';
+    let cursor = 0;
 
-      inputBuf += text;
+    return new Promise<string>((resolve: any, reject: any) => {
+      // This code assumes that the data received is a single
+      // character string, unless the first character is escape
+      // (27), in which case it may be followed by escaped data
+      // flow characters.
+      const listener = this.term.onData((data: string) => {
+        const code = data.charCodeAt(0);
 
-      if (text === '\r') {
-        this.term.write('\r\n');
+        switch (code) {
+          case 27:
+            switch (data.substr(1)) {
+              case '[A':
+                // Up arrow: should go back in history
+                break;
 
-        if (this.context && this.context.inputStream) {
-          this.context.inputStream.write(inputBuf);
-          inputBuf = '';
+              case '[B':
+                // Down arrow: should go forward in history
+                break;
+
+              case '[3~':
+                // Delete
+                this.term.write('SORRY DELETE KEY IS NOT YET HANDLED!');
+                break;
+
+              case '[F':
+                // End
+                this.term.write(moveRight.repeat(buf.length - cursor));
+                cursor = buf.length;
+                break;
+
+              case '[H':
+                // Home
+                this.term.write(moveLeft.repeat(cursor));
+                cursor = 0;
+                break;
+
+              case '[C':
+                // Right arrow
+                if (cursor < buf.length) {
+                  cursor++;
+                  this.term.write(data);
+                }
+                break;
+
+              case '[D':
+                // Left arrow
+                if (cursor > 0) {
+                  cursor--;
+                  this.term.write(data);
+                }
+                break;
+
+              default:
+                this.term.write(`ESC "${data.substr(1)}"`);
+            }
+            break;
+
+          case 13:
+            this.term.write(data);
+            listener.dispose();
+            resolve(buf);
+            break;
+
+          case 127:
+            // Backspace
+            if (cursor > 0) {
+              if (cursor === buf.length) {
+                buf = buf.substr(0, buf.length - 1);
+                cursor--;
+                this.term.write(`${moveLeft} ${moveLeft}`);
+              } else {
+                this.term.write(moveLeft);
+                this.term.write(buf.substr(cursor) + ' ');
+                this.term.write(moveLeft.repeat(buf.length - cursor + 1));
+                buf = buf.substr(0, cursor - 1) + buf.substr(cursor);
+                cursor--;
+              }
+            }
+            break;
+
+          default:
+            if (cursor === buf.length) {
+              buf = buf + data;
+              this.term.write(data);
+            } else {
+              const tail = data + buf.substr(cursor);
+              this.term.write(tail);
+              this.term.write('\x1B[D'.repeat(tail.length - 1));
+              buf = buf.substr(0, cursor) + tail;
+            }
+            cursor += data.length;
+            break;
         }
-      }
+
+        console.log(`New input: length=${data.length} code=${code} buf=${buf}`);
+      });
     });
   }
 
@@ -53,35 +190,37 @@ class BasicTerminal {
 
     this.term.open(el);
 
-    const env = setupEnvironment();
+    const support = {
+      finalize: () => {},
+      open: async (filename: string, mode: string, channel: number) => {},
+      close: async (channel: number) => {},
+      print: async (channel: number, value: string) => {
+        const value2 = value.replace(/\n/g, '\n\r');
+        await this.print(value2);
+      },
+      printError: async (message: string) => {
+        const message2 = message.replace(/\n/g, '\n\r');
+        await this.print(forcedChalk.redBright(message2) + '\n\r');
+      },
+      readLine: async (channel: number) => {
+        const text = await this.readInput();
+        await this.print('\n');
+        return text;
+      },
+    };
+
+    const env = setupEnvironment('', support);
     this.program = env.program;
     this.context = env.context;
-
-    // Input from user/keyboard to interpreter
-    this.context.inputStream = new Stream();
-
-    // Output from interpreter, to be presented on screen
-    this.context.outputStream = new Stream();
-    this.context.outputStream.on('data', (data: string) => {
-      console.log(`Output stream data: [${data}]`);
-      const data2 = data.replace(/\n/g, '\n\r');
-      this.term.write(data2);
-    });
-
-    // Error from interpreter, to be presented on screen
-    this.context.errorStream = new Stream();
-    this.context.errorStream.on('data', (data: string) => {
-      console.log(`Error stream data: [${data}]`);
-      const data2 = data.replace(/\n/g, '\n\r');
-      this.term.write(forcedChalk.redBright(data2));
-    });
 
     fetch('/stuga.bas')
       .then((r: any) => {
         console.log(r);
         return r.text();
       })
-      .then((r: any) => this.program!.loadFromString(r, this.context!))
+      .then((r: any) => {
+        this.program = parse(r);
+      })
       .then(() => shell(this.program!, this.context!));
   }
 }
