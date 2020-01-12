d328d9a41a2c97ceda8749476a704357fb8b14ce
Jonatan Magnusson <jonatan.magnusson@above.se>
2020-01-12 12:55:51 +0100

    Disabled the intro animation

--- src/App.tsx
+++ src/App.tsx
@@ -4,46 +4,43 @@ import './App.css';
 import 'xterm/css/xterm.css';
 import BasicTerminal from './BasicTerminal';
 
-const intro = [
-  '     _____ __',
-  '   /  ___ V  |',
-  '  |  (___\\___|',
-  '   \\_______  \\',
-  '   ________)  |',
-  '  |__________/',
-];
-
 const startAnimation = async (term: BasicTerminal) => {
   const sleep = async (ms: number) => {
     return new Promise((resolve, reject) => {
       setTimeout(() => resolve(), ms);
     });
   };
-  term.term.focus();
-  const prevDelay = term.slowDownDelay;
-  const prevDelayBatch = term.slowDownBatch;
 
-  term.slowDownDelay = 84;
-  term.slowDownBatch = 1;
-  await sleep(2100);
-  await term.print('ATDT127001\r\n');
+  const introResponse = await fetch('/STUGA.ANS');
+  const intro = await introResponse.text();
 
-  var context = new AudioContext();
-  context.resume().then(() => {
-    const audio = new Audio('connect.ogg');
-    console.log(audio);
-    audio.play();
-  });
+  if (false) {
+    term.term.focus();
+    const prevDelay = term.slowDownDelay;
+    const prevDelayBatch = term.slowDownBatch;
 
-  // await sleep(28000);
-  term.slowDownDelay = 1;
-  term.slowDownBatch = 2;
-  await term.print('CONNECT 9600\r\n');
+    term.slowDownDelay = 84;
+    term.slowDownBatch = 1;
+    await sleep(2100);
+    await term.print('ATDT127001\r\n');
 
-  term.slowDownDelay = prevDelay;
-  term.slowDownBatch = prevDelayBatch;
+    var context = new AudioContext();
+    context.resume().then(() => {
+      const audio = new Audio('connect.ogg');
+      console.log(audio);
+      audio.play();
+    });
 
-  await term.print(intro.join('\n\r') + '\n\r');
+    // await sleep(28000);
+    term.slowDownDelay = 1;
+    term.slowDownBatch = 2;
+    await term.print('CONNECT 9600\r\n');
+
+    term.slowDownDelay = prevDelay;
+    term.slowDownBatch = prevDelayBatch;
+  }
+
+  await term.print(intro); //.join('\n\r') + '\n\r');
 };
 
 const ReactBasicTerminal = ({ cols, rows }: { cols: number; rows: number }) => {
