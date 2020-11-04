c06704ffb80010b3dcc080b93c3ffd0735b73eb3
Jonatan Magnusson <jonatan.magnusson@above.se>
2020-11-04 22:31:05 +0100

    Final commit

--- src/components/Terminal.vue
+++ src/components/Terminal.vue
@@ -3,25 +3,51 @@
 </template>
 
 <script lang="ts">
-// import { Component, Vue } from 'vue-property-decorator';
+import Vue, { PropType } from 'vue';
 import BasicTerminal from '../BasicTerminal';
-// import intro from '../assets/stuga.ans';
-// const intro = 'hejhej';
 
-export default {
+let gTermHack: BasicTerminal | null = null;
+
+const Terminal = Vue.extend({
+  props: {
+    autorun: { type: Boolean },
+    preload: { type: Array as PropType<string[]> },
+    allCaps: { type: Boolean },
+    term: BasicTerminal
+  },
+
   mounted() {
     const term = new BasicTerminal({ cols: 80, rows: 25 });
+    gTermHack = term;
+
     const container = document.getElementById('term-container');
+
     if (!container) {
       throw new Error('Container not found');
+    }
+
+    for (const inp of this.preload || []) {
+      term.addInput(inp);
     }
 
     term.open(container as HTMLDivElement).then(() => {
       term.fit();
       term.start();
+      term.focus();
     });
+  },
+
+  watch: {
+    allCaps: {
+      immediate: true,
+      handler(val) {
+        gTermHack!.setAllCaps(val);
+      }
+    }
   }
-};
+});
+
+export default Terminal;
 </script>
 
 <style>
